8e9fa53c489668a110cb320e29076e9d26d07463
esperanzaWG <myfrick57@hotmail.com>
2022-02-24 10:29:31 -0500

    axios

--- endpoints/dealers.js
+++ endpoints/dealers.js
@@ -1,12 +1,12 @@
 
 const { Router} = require('express');
 const router = Router();
-const got = require('got');
+const axios = require('axios');
 
 const api_domain='https://stagingapi.coolcalc.com';
 // Enter your API credentials
-const ClientId = '';
-const APIKey = '';
+const ClientId = 'DCNE';
+const APIKey = 'letmein';
 
 //raw body (this is only necessary for post and put methods)
 var bodyParser = require('body-parser');
@@ -35,136 +35,100 @@ const coolcalcAuthentication = function(){
  * and the CoolCalc API's response is forwarded unchanged to the browser. 
  */
 router.get( '*', async(req, res) => {
-	// To-do:
-    // Implement your own code here to check that the accountNr in the URL /dealers/accountNr/.... 
-    // corresponds to the current user/session info.
-    // This is to prevent some dishonest user from accessing someone else's project list.
-    // ...
-    // If the account nr in the REST URL does not correspond to the session user, respond with a 401 "are you trying to hack me" code.
-    // ...
-    	
-    // Remove URL segments specific to our local entry point.
+
    const myUrl = req.originalUrl;
    const newURL = myUrl.replace('/coolcalc/client', '' );
 
     // Get data from CoolCalc API.  
-    var options = {
-        'url': `${api_domain}${newURL}`,
-        'headers': {
-            'Authorization': `Basic ${coolcalcAuthentication()}`
-        }
-    };
-    let response = await got.get(options);
+    try {
+        var response = await axios.get(`${api_domain}${newURL}`,  {
+            headers:{
+                Authorization: `Basic ${coolcalcAuthentication()}`
+            }
+        });
+        // Output to the browser.
+        res.setHeader('Content-Type', response.headers['content-type']);
+        res.setHeader('allow', response.headers['allow']);
+        res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
+        res.jsonp(JSON.parse(response.body));
+        
 
-    // Output to the browser.
-    res.setHeader('Content-Type', response.headers['content-type']);
-    res.setHeader('allow', response.headers['allow']);
-    res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
-    res.jsonp(JSON.parse(response.body));
+    } catch(err){
+        res.json({
+            error: err
+        }); 
+    }
+
 
 } );
 
 
 router.post( '*', raw,async (req, res) => {
-	// To-do:
-    // Implement your own code here to check that the accountNr in the URL /dealers/accountNr/.... 
-    // corresponds to the current user/session info.
-    // This is to prevent some dishonest user from accessing someone else's project list.
-    // ...
-    // If the account nr in the REST URL does not correspond to the session user, respond with a 401 "are you trying to hack me" code.
-    // ...
 
-    // Remove URL segments specific to our local entry point.
     const myUrl = req.originalUrl;
     const newURL = myUrl.replace('/coolcalc/client', '' );
-    
-    // Post data to CoolCalc API and read the response.
-    var options = {
-        'url': `${api_domain}${newURL}`,
-        'headers': {
-        'Authorization': `Basic ${coolcalcAuthentication()}`
-            },
-        'body':  `${req.body}`
-    };
+
     res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
 
-    // Make the request
-    await  got.post(options).then((doc) => {
-		// Output to the browser.
-        res.setHeader('Content-Type', doc.headers['content-type']);
-        res.setHeader('allow', doc.headers['allow']); 
-        res.setHeader('location', doc.headers['location']); 
-        return res.status(doc.statusCode).json(JSON.parse(doc.body));
-    }).catch(err => {
+    try{
+        await axios.post(`${api_domain}${newURL}`,  {
+            headers:{
+                Authorization: `Basic ${coolcalcAuthentication()}`
+            },
+            body: `${req.body}`
+        }).then((doc) => {
+            // Output to the browser.
+            res.setHeader('Content-Type', doc.headers['content-type']);
+            res.setHeader('allow', doc.headers['allow']); 
+            res.setHeader('location', doc.headers['location']); 
+            return res.status(doc.statusCode).json(JSON.parse(doc.body)) });
+    } catch (err) {
         res.setHeader('Content-Type', err.response.headers['content-type']);
         return res.status(err.response.statusCode).json(JSON.parse(err.response.body));
-    })
+    }
 } );
 
 
 router.put( '*', raw,async(req, res) => {
-	// To-do:
-    // Implement your own code here to check that the accountNr in the URL /dealers/accountNr/.... 
-    // corresponds to the current user/session info.
-    // This is to prevent some dishonest user from accessing someone else's project list.
-    // ...
-    // If the account nr in the REST URL does not correspond to the session user, respond with a 401 "are you trying to hack me" code.
-    // ...
 
-    // Remove URL segments specific to our local entry point.
     const myUrl = req.originalUrl;
     const newURL = myUrl.replace('/coolcalc/client', '' );
  
-    // Post data to CoolCalc API and read the response.
-     var options = {
-         'url': `${api_domain}${newURL}`,
-         'headers': {
-             'Authorization': `Basic ${coolcalcAuthentication()}`
-             },
-             'body':  `${req.body}`
-     };
-  
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
 
-    // Make the request
-    await  got.put(options).then((doc) => {
-		// Output to the browser.
+    try{
+        await axios.put(`${api_domain}${newURL}`,  {
+            headers:{
+                Authorization: `Basic ${coolcalcAuthentication()}`
+            },
+            body: `${req.body}`
+        }).then((doc) => {
+            // Output to the browser.
         res.setHeader('Content-Type', doc.headers['content-type']);
         res.setHeader('allow', doc.headers['allow']); 
-        return res.status(doc.statusCode).json(JSON.parse(doc.body));
-    }).catch(err => {
+        return res.status(doc.statusCode).json(JSON.parse(doc.body))});
+    } catch (err) {
         res.setHeader('Content-Type', err.response.headers['content-type']);
         return res.status(err.response.statusCode).json(JSON.parse(err.response.body));
-    })
+    }
 });
 
 
 router.delete( '*', (req, res) => {
-	// To-do:
-    // Implement your own code here to check that the accountNr in the URL /dealers/accountNr/.... 
-    // corresponds to the current user/session info.
-    // This is to prevent some dishonest user from accessing someone else's project list.
-    // ...
-    // If the account nr in the REST URL does not correspond to the session user, respond with a 401 "are you trying to hack me" code.
-    // ...
 
     // Remove URL segments specific to our local entry point.
     const myUrl = req.originalUrl;
     const newURL = myUrl.replace('/coolcalc/client', '' );
-
-    //sending the options to be able to make a new request
-     var options = {
-         'url': `${api_domain}${newURL}`,
-         'headers': {
-             'Authorization': `Basic ${coolcalcAuthentication()}`
-        }
-     };
   
     // Make the request
     try{
-        got.delete(options);
+        axios.delete(`${api_domain}${newURL}`,  {
+            headers:{
+                Authorization: `Basic ${coolcalcAuthentication()}`
+            }
+        })
         res.sendStatus(204);
     }
     catch (error) {
--- endpoints/mj8_report.js
+++ endpoints/mj8_report.js
@@ -1,15 +1,15 @@
 const { Router} = require('express');
 const router = Router();
 
-const got = require('got');
+const axios = require('axios');
 
 // The URL for the MJ8 report at the CoolCalc API.
 // MJ8 report ids are not guessable so they can be safely retrieved outside the normal UI flow.
 const APIUrl = "https://stagingapi.coolcalc.com/staging/MJ8Reports/?reportId=";
 
 // Enter your API credentials
-const ClientId = '';
-const APIKey = '';
+const ClientId = '111';
+const APIKey = '1626443386';
 
 // coolcalcAuthentication returns a Base64 encoded HTTP Basic authentication string.
 const coolcalcAuthentication = function(){
@@ -22,20 +22,33 @@ router.get( '*', async(req, res) => {
     const reportId = req.params.reportId;   
 
     // Get data from CoolCalc API.
-   var options = {
+   /* var options = {
         'method': 'GET',
         'url': `${APIUrl}${reportId}&rev=latest`,
         'headers': {
             'Authorization': `Basic ${coolcalcAuthentication()}`
             }
-    };
-    let response = await got(options);
+    }; */
 
-    // Output to the browser.
-    res.setHeader('Content-Type', response.headers['content-type']);
-    res.setHeader('allow', response.headers['allow']);
-    res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
-    res.jsonp(JSON.parse(response.body));
+    try {
+        var response = await axios.get(`${api_domain}${newURL}`,  {
+            headers:{
+                Authorization: `Basic ${coolcalcAuthentication()}`
+            }
+        });
+        // Output to the browser.
+        res.setHeader('Content-Type', response.headers['content-type']);
+        res.setHeader('allow', response.headers['allow']);
+        res.setHeader('Access-Control-Expose-Headers', 'Location, Allow');
+        res.jsonp(JSON.parse(response.body));
+
+    } catch(err){
+        res.json({
+            error: err
+        }); 
+    }
+
+    
 
 } );
 
